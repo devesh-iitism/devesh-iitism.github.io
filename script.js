3255daee467594530409f8d91dbb5a9c093866d2
Devesh Chaturvedi <dcdev001@gmail.com>
2020-10-22 05:02:13 +0530

    add files

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	$(function () {
+        $(document).scroll(function () {
+        	var $nav = $("#mainNavbar")
+            var $pr = $("#projects");
+            var $d = $(".dc");
+            if ($(this).scrollTop() > $nav.height())
+            {
+            	$nav.css({top : '-50px'});
+            	$nav.removeClass("bg-dark1");	
+            }
+
+            if ($(this).scrollTop() < $nav.height())
+            {
+            	$nav.css({top : '0'});
+            	$nav.removeClass("bg-dark1");	
+            }
+
+            if ($(this).scrollTop() > $d.height())
+            {
+            	$nav.css({top : '0'});
+            	$nav.addClass("bg-dark1");	
+            }
+        });
+    });
+
+    $(window).scroll(function() {
+        var scrollDistance = $(window).scrollTop();
+        $('.page-section').each(function(i) {
+                console.log($(this).position().top + "hi");
+                console.log(scrollDistance + "hello")
+                if ($(this).position().top <= scrollDistance + 120) {
+                        $('.nav-item a.active').removeClass('active');
+                        $('.nav-item a').eq(i).addClass('active');
+                }
+        });
+    }).scroll();
+}
